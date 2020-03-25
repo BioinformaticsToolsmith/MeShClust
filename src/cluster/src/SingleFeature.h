ca9f5eee66a058e96de11d68e6e93c9de07b807d
Árpád Goretity <h2co3@h2co3.org>
2020-03-25 20:22:15 +0100

    Fix some of the more dangerous / less obnoxious warnings

--- src/cluster/src/SingleFeature.h
+++ src/cluster/src/SingleFeature.h
@@ -8,9 +8,9 @@ template<class T>
 class SingleFeature {
 public:
 	SingleFeature(std::function<double(Point<T>*, Point<T>*)> f, bool is_sim_=true)
-		: raw(f), is_sim(is_sim_), min_set(false), max_set(false) {}
+		: raw(f), is_sim(is_sim_), max_set(false), min_set(false) {}
 	SingleFeature(std::function<double(Point<T>*, Point<T>*, const vector<int>&, const vector<int>&)> f, vector<int> rrv, vector<int> rrc, bool is_sim_=true)
-		: rraw(f), is_sim(is_sim_), min_set(false), max_set(false), rv(rrv), rc(rrc) {}
+		: rraw(f), rv(rrv), rc(rrc), is_sim(is_sim_), max_set(false), min_set(false) {}
 	void normalize(const vector<pair<Point<T>*,Point<T>*> > &pairs);
 	double operator()(Point<T>*, Point<T>*) const;
 	double min, max;
